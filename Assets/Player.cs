4b13e279a768bac798b4e374c67bc3ee738ad0de
brucenielson <brucenielson@users.noreply.github.com>
2020-08-10 13:50:24 -0600

    First

---
+++ Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public int maxHealth = 1000;
+    public int currentHealth;
+    private Scene scene;
+
+    public HealthBar healthbar;
+    public Collider collider;
+
+	// Start is called once per frame
+	void Start ()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    //teleport after death
+    private void Update()
+    {
+        if (currentHealth < 0)
+        {
+            SceneManager.LoadScene("waterTest");
+        }
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "WaterProNighttime")
+        {
+            TakeDamage(100);
+        }
+          
+    }
+    
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthbar.SetHealth(currentHealth);
+    }
+}
